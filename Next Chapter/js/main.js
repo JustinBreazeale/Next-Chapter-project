3594e01bf91065f63124a92b9506f6dcb53d8718
Justin Breazeale <justinbreazeale1219@gmail.com>
2024-10-12 06:55:54 -0600

    Works in javascript and HTML files edits picture function added modal

--- Next Chapter/js/main.js
+++ Next Chapter/js/main.js
@@ -1,71 +1,48 @@
-const leftPic = document.getElementById('leftPic')
-const rightPic = document.getElementById('rightPic')
-// Picture creation declarations---------------------->
-const div = document.getElementById('topRow')
-const arz = document.createElement('img')
-arz.setAttribute('src', './assets/arizona.png')
-const dal = document.createElement('img')
-dal.setAttribute('src', './assets/dallas.png')
-const wash = document.createElement('img')
-wash.setAttribute('src', './assets/washington.png')
-const phi = document.createElement('img') 
-phi.setAttribute('src', './assets/philedelphia.png')
-const nyg = document.createElement('img') 
-nyg.setAttribute('src', './assets/newYorkG.png')
-const bal = document.createElement('img') 
-bal.setAttribute('src', './assets/baltimore.png')
-const buff = document.createElement('img') 
-buff.setAttribute('src', './assets/buffalo.png')
-const mia = document.createElement('img') 
-mia.setAttribute('src', './assets/miami.png')
-const tb = document.createElement('img')
-tb.setAttribute('src', './assets/tampa.png') 
-const car = document.createElement('img')
-car.setAttribute('src', './assets/carolina.png') 
-const den = document.createElement('img') 
-den.setAttribute('src', './assets/denver.png')
-const lv = document.createElement('img')
-lv.setAttribute('src', './assets/lasVegas.png') 
-const sf = document.createElement('img')
-sf.setAttribute('src', './assets/sanFran.png') 
-const kc = document.createElement('img') 
-kc.setAttribute('src', './assets/kansasCity.png')
-const lac = document.createElement('img')
-lac.setAttribute('src', './assets/losAngeles.png') 
-const lar = document.createElement('img')
-lar.setAttribute('src', './assets/losAngelesRams.png') 
-const sea = document.createElement('img') 
-sea.setAttribute('src', './assets/seattle.png')
-const nyj = document.createElement('img')
-nyj.setAttribute('src', './assets/newYorkJ.png') 
-const jax = document.createElement('img')
-jax.setAttribute('src', './assets/jacksonville.png') 
-const hou = document.createElement('img')
-hou.setAttribute('src', './assets/houston.png')
-const tenn = document.createElement('img')
-tenn.setAttribute('src', './assets/tennesse.png') 
-const det = document.createElement('img')
-det.setAttribute('src', './assets/detroit.png') 
-const gb = document.createElement('img') 
-gb.setAttribute('src', './assets/greenbay.png')
-const minn = document.createElement('img')
-minn.setAttribute('src', './assets/minnesota.png') 
-const chi = document.createElement('img')
-chi.setAttribute('src', './assets/chicago.png') 
-const atl = document.createElement('img')
-atl.setAttribute('src', './assets/atlanta.png') 
-const pitt = document.createElement('img')
-pitt.setAttribute('src', './assets/pittsburgh.png')
-const no = document.createElement('img')
-no.setAttribute('src', './assets/newOrleans.png') 
-const ne = document.createElement('img') 
-ne.setAttribute('src', './assets/newEngland.png')
-const ind = document.createElement('img') 
-ind.setAttribute('src', './assets/indy.png')
-const cin = document.createElement('img')
-cin.setAttribute('src', './assets/cinncinatti.png') 
-const cle = document.createElement('img')
-cle.setAttribute('src', './assets/clevland.png')
+const leftPicDiv = document.getElementById('leftPicDiv')
+const rightPicDiv = document.getElementById('rightPicDiv')
+const leftImage = document.getElementById('leftImage')
+const rightImage = document.getElementById('rightImage')
+const formDiv = document.getElementById('formDiv')
+const form = document.getElementById('regForm')
+const formItem = document.getElementsByClassName('formItem')
+const itemArr = Array.from(formItem)
+// itemArr.forEach((item) => {
+//     item.style.display = 'none'
+// })
+// formItem.style.display = 'none'
+// Picture source declarations---------------------->
+const arz = './assets/arizona.png'
+const dal = './assets/dallas.png'
+const wash = './assets/washington.png'
+const phi = './assets/philedelphia.png'
+const nyg = './assets/newYorkG.png'
+const bal = './assets/baltimore.png'
+const buff = './assets/buffalo.png'
+const mia = './assets/miami.png'
+const tb = './assets/tampa.png'
+const car = './assets/carolina.png'
+const den = './assets/denver.png'
+const lv = './assets/lasVegas.png'
+const sf = './assets/sanFran.png'
+const kc = './assets/kansasCity.png'
+const lac = './assets/losAngeles.png'
+const lar = './assets/losAngelesRams.png'
+const sea = './assets/seattle.png'
+const nyj = './assets/newYorkJ.png'
+const jax = './assets/jacksonville.png'
+const hou = './assets/houston.png'
+const tenn = './assets/tennesse.png'
+const det = './assets/detroit.png'
+const gb = './assets/greenbay.png'
+const minn = './assets/minnesota.png'
+const chi = './assets/chicago.png'
+const atl = './assets/atlanta.png'
+const pitt = './assets/pittsburgh.png'
+const no = './assets/newOrleans.png'
+const ne = './assets/newEngland.png'
+const ind = './assets/indy.png'
+const cin = './assets/cinncinatti.png'
+const cle = './assets/clevland.png'
 // Picture Array------------------------------------> 
 const teamPicArr = [
     arz,
@@ -109,24 +86,18 @@ function ranNum(min, max){
 // Functions for placing and removing pictures---------------->
 function placerLeft(){
     const numPick = ranNum(0, teamPicArr.length)
-    console.log(numPick)
+    // console.log(numPick)
     const finalPick = teamPicArr[numPick]
-    leftPic.append(finalPick)
-    finalPick.setAttribute('class', 'pic')
-    function gone(){
-        finalPick.remove()
-    }
-    setTimeout(gone, 1500)
+    leftImage.setAttribute('class', 'pic')
+    leftImage.setAttribute('src', finalPick)
+    // console.log(finalPick)
 }
 function placerRight(){
     const numPick = ranNum(0, teamPicArr.length)
     const finalPick = teamPicArr[numPick]
-    rightPic.append(finalPick)
-    finalPick.setAttribute('class', 'pic')
-    function gone(){
-        finalPick.remove()
-    }
-    setTimeout(gone, 1500)
+    rightImage.setAttribute('class', 'pic')
+    rightImage.setAttribute('src', finalPick)
+    // console.log(finalPick)
 }
 placerLeft()
 placerRight()
